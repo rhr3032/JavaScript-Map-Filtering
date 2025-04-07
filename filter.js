c6ca39bcd9a25dbaa28b38944aab86ef04badf5e
rhr3032 <89746277+rhr3032@users.noreply.github.com>
2025-04-07 11:52:45 +0600

    2nd

--- filter.js
+++ filter.js
@@ -19,3 +19,15 @@ let filteredElements = arr.filter(function(x) {
 });
 
 console.log(filteredElements); // Output: [5, 6, 7, 8]
+
+
+
+
+// Keep only the values 5, 6, 7, and 8
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let filteredElements = arr.filter((x) => {
+    return x == 5 || x == 6 || x == 7 || x == 8;
+});
+
+console.log(filteredElements); // Output: [5, 6, 7, 8]
